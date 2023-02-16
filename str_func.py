813090122c4947c074e591beb465cd844dcca647
Max <maks_orlov_2015@mail.ru>
2023-02-16 20:55:47 +1000

    add def up_first_letter and docstring

--- str_func.py
+++ str_func.py
@@ -3,3 +3,9 @@ def up_word():
     word = input()
     word_up = word.upper()
     return word_up
+
+def up_first_letter():
+    '''возвращает слово с заглавной буквой'''
+    word = input()
+    first_letter_up = word.title()
+    return first_letter_up
